a2499cc3d727521fe9b645bc3c2b503b7453ad7d
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-01-30 07:42:14 +0000

    fix issue with dagger connect

--- src/dagger/lib.ts
+++ src/dagger/lib.ts
@@ -1,31 +1,36 @@
-import Client, {
-  Directory,
-  DirectoryID,
-  Secret,
-  SecretID,
-} from "../../deps.ts";
+import { Directory, DirectoryID, Secret, SecretID } from "../../deps.ts";
+import { Client } from "../../sdk/client.gen.ts";
 
-export const getDirectory = (
+export const getDirectory = async (
   client: Client,
   src: string | Directory | undefined = "."
 ) => {
-  if (typeof src === "string" && src.startsWith("core.Directory")) {
-    return client.directory({
-      id: src as DirectoryID,
-    });
+  if (typeof src === "string") {
+    try {
+      const directory = client.loadDirectoryFromID(src as DirectoryID);
+      await directory.id();
+      return directory;
+    } catch (_) {
+      return client.host().directory(src);
+    }
   }
   return src instanceof Directory ? src : client.host().directory(src);
 };
-
-export const getSonarToken = (client: Client, token?: string | Secret) => {
+export const getSonarToken = async (
+  client: Client,
+  token?: string | Secret
+) => {
   if (Deno.env.get("SONAR_TOKEN")) {
     return client.setSecret("SONAR_TOKEN", Deno.env.get("SONAR_TOKEN")!);
   }
   if (token && typeof token === "string") {
-    if (token.startsWith("core.Secret")) {
-      return client.loadSecretFromID(token as SecretID);
+    try {
+      const secret = client.loadSecretFromID(token as SecretID);
+      await secret.id();
+      return secret;
+    } catch (_) {
+      return client.setSecret("SONAR_TOKEN", token);
     }
-    return client.setSecret("SONAR_TOKEN", token);
   }
   if (token && token instanceof Secret) {
     return token;
